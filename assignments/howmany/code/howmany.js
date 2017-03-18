e23e07e6b4944927d1020c3a261c65f33b429b40
METROSTATE\36BDPA6 <lukejohnson@mailinator.com>
2017-03-18 13:58:24 -0500

    ciommit

--- assignments/howmany/code/howmany.js
+++ assignments/howmany/code/howmany.js
@@ -1,3 +1,38 @@
 function doLevel1(){
-	alert("hi");
+	var answer = prompt("Enter you answer")
+	alert("2"); 
+	hide("level1"); 
+	show("level2"); 
+	
+}
+
+function doLevel2(){
+    var answer = prompt("Enter you answer")
+	alert("hii");
+	hide("level2");
+	show("level3");	
+	
+}
+
+function doLevel3(){
+	var answer = prompt("Enter you answer")
+	alert("hhi");
+	
+}
+
+function show(id){
+	var el = document.getElementById(id);
+	el.classList.remove("hidden");
+}
+
+function hide(id){
+	var el = document.getElementById(id);
+	el.classList.add("hidden")
+}
+
+var score = 0;
+
+function addpoints(points){
+	score = score + points;
+	document.getElementById("score").innerHTML = score;
 }
